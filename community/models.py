2a454b9d4d2e3ebc93a756aec61b0c3e1a6b2eb1
Rob Redpath <rob@digital-saints.com>
2016-03-29 11:36:09 +0100

    Add __str__ methods to models

--- community/models.py
+++ community/models.py
@@ -6,7 +6,18 @@ class Message(models.Model):
 	sms_has_been_sent = models.BooleanField(default=False, editable=False)
 	email_has_been_sent = models.BooleanField(default=False, editable=False)
 
+	def __str__(self):
+		return "Long message ({}): {} Short message ({}): {}".format('Sent' if self.email_has_been_sent else 'Unsent', 
+																	self.message_text_long,
+																	'Sent' if self.sms_has_been_sent else 'Unsent',
+																	self.message_text_short
+		 )
+
+
 class Event(models.Model):
 	start_time = models.DateTimeField()
 	end_time = models.DateTimeField()
 	title = models.CharField(max_length=250)
+
+	def __str__(self):
+		return "{}: {} to {}".format(title, start_time, end_time)
